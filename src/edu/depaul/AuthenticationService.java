fc57de90b0e016d2e820ab8f3eccd90b8583ace6
Shardul Janaskar <sharduljanaskar@Sharduls-Air.attlocal.net>
2024-06-04 19:09:01 -0500

    implemented authentication service

--- src/edu/depaul/AuthenticationService.java
+++ src/edu/depaul/AuthenticationService.java
@@ -7,9 +7,16 @@ public class AuthenticationService {
     private Map<String, User> users = new HashMap<>();
 
     public boolean register(String username, String password) {
+        if (users.containsKey(username)) {
+            return false;
+        }
+        users.put(username, new User(username, password));
+        return true;
     }
 
     public boolean login(String username, String password) {
+        User user = users.get(username);
+        return user != null && user.getPassword().equals(password);
     }
 }
 
