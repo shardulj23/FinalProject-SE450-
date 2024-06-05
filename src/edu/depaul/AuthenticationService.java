4f0ea466d69bd175bd950a9e598bfd3f84d732c0
Shardul Janaskar <sharduljanaskar@Sharduls-Air.attlocal.net>
2024-06-04 19:04:19 -0500

    working on authentication service

---
+++ src/edu/depaul/AuthenticationService.java
@@ -0,0 +1,15 @@
+package edu.depaul;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AuthenticationService {
+    private Map<String, User> users = new HashMap<>();
+
+    public boolean register(String username, String password) {
+    }
+
+    public boolean login(String username, String password) {
+    }
+}
+
