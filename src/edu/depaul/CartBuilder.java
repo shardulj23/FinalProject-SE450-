3890a8436fc8ca3feb76164d6a21a4653c5f042a
Shardul Janaskar <sharduljanaskar@Sharduls-Air.attlocal.net>
2024-06-04 20:09:51 -0500

    implemented add and remove product

--- src/edu/depaul/CartBuilder.java
+++ src/edu/depaul/CartBuilder.java
@@ -6,6 +6,16 @@ public class CartBuilder {
     public CartBuilder() {
         cart = ShoppingCart.getInstance();
     }
+
+    public CartBuilder addProduct(Product product) {
+        cart.addProduct(product);
+        return this;
+    }
+
+    public CartBuilder removeProduct(Product product) {
+        cart.removeProduct(product);
+        return this;
+    }
     
     public ShoppingCart build() {
         return cart;
