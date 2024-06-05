4c40d179556d442323591b0087e57766deb897e6
Shardul Janaskar <sharduljanaskar@Sharduls-Air.attlocal.net>
2024-06-04 19:49:29 -0500

    working on CartBuilder

--- src/edu/depaul/CartBuilder.java
+++ src/edu/depaul/CartBuilder.java
@@ -1,6 +1,14 @@
 package edu.depaul;
 
 public class CartBuilder {
+    private ShoppingCart cart;
+
+    public CartBuilder() {
+        cart = ShoppingCart.getInstance();
+    }
     
+    public ShoppingCart build() {
+        return cart;
+    }
 }
 
